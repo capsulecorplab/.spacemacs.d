bd301cfb355a10eb26c98b4a55acbd9ca93b26ee
Sean Marquez <capsulecorplab@gmail.com>
2025-06-12 11:03:07 -0700

    update persistent volume path to home directory

--- run_emacs_from_podman.sh
+++ run_emacs_from_podman.sh
@@ -8,9 +8,9 @@ podman run --rm\
  -e DISPLAY\
  -v $HOME/.emacs.d:/root/.emacs.d\
  -v $HOME/.spacemacs.d:/root/.spacemacs.d\
- -v $HOME/Dropbox/katra:/root/katra\
- -v $HOME/Dropbox/org-roam:/root/org-roam\
- -v $HOME/Dropbox/org:/root/org\
+ -v $HOME/katra:/root/katra\
+ -v $HOME/org-roam:/root/org-roam\
+ -v $HOME/org:/root/org\
  -v /tmp/.X11-unix:/tmp/.X11-unix\
  -w /root\
  docker.io/silex/emacs:28.2-dev
